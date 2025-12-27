e9a3702540355622bf9a5fe97de96e15e11fa22b
felipemartel <felipe.elocance@gmail.com>
2025-12-27 14:07:42 +0100

    al menos esta poronga funca

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,14 @@
-# No se porque este codigo funciona para el reverendo ojete
+# Ahora esta cagada funciona un poco mejor
+
+import sys
+from pathlib import Path
+
+project_root = Path("/home/felipeelocance/exercice-github-vscode")
+sys.path.insert(0, str(project_root))
 
 from src.utils import dire_bonjour
 
 def test_dire_bonjour():
     dire_bonjour("Test")
+
+test_dire_bonjour()
